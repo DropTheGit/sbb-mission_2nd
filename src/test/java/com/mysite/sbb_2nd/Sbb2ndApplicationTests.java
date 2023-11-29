770774a4daef7180cf6f57dd58a2ba647423272a
Sanghyuk Park <dropthegit@gmail.com>
2023-11-29 22:05:22 +0900

    2nd setting

---
+++ src/test/java/com/mysite/sbb_2nd/Sbb2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb2ndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
